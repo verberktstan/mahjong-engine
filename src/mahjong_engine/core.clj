af50deaa98a0c412556e360e7ac0feae85d332ce
Stan Verberkt <verberktstan@gmail.com>
2017-08-02 23:08:09 +0200

    Updated implementation of the tiles and added fn guess-tile, for guessing tiles based on incomplete user input

--- src/mahjong_engine/core.clj
+++ src/mahjong_engine/core.clj
@@ -1,5 +1,8 @@
-(ns mahjong-engine.core)
+(ns mahjong-engine.core
+ (:require [clojure.string :as s]))
 
+; TODO: Split implementation of tiles to suits (bamboos, dots & signs) and bonus (winds, dragins, seasons, flowers). Each tile has a name and a value.
+; Functions concerned: make-get-suit-fn, make-get-rank-fn, make-same-suit-fn, tiles and the tile-definitions map
 ; TODO: Implement a fn that returns all the close tiles in hand (same suit, within chow range for standard suits)
 ; TODO: Implement a scoring system & notion of chows, pungs and kongs.
 ; TODO: Implement generic interaction functions (inform / prompt) for ui
@@ -7,23 +10,50 @@
 ; TODO: Implement the execution of a round
 ; TODO: Separate functions into different namespaces/files
 
-; Definition of available tiles. Can be used as argument to make-get-suit-fn and make-get-rank-fn. It should be possible (in the future) to supply different tile definition sets for a new game.
-(def tile-definitions
- {:b {:suit "bamboo"
-      :ranks (apply hash-map
-              (apply interleave (repeat 2 (range 1 10))))}
-  :c {:suit "circle"
-      :ranks (apply hash-map
-              (apply interleave (repeat 2 (range 1 10))))}
-  :d {:suit "dragon"
-      :ranks (apply hash-map
-              (interleave [\g \r \w] ["green" "red" "white"]))}
-  :s {:suit "sign"
-      :ranks (apply hash-map
-              (apply interleave (repeat 2 (range 1 10))))}
-  :w {:suit "wind"
-      :ranks (apply hash-map
-              (interleave [\e \n \s \w] ["east" "north" "south" "west"]))}})
+; Should probably convert this to a smart regex match sometime... Also this is not needed in the core lib.
+(defn guess-tile
+ [input]
+ (let [n (cond
+             (and
+              (s/includes? input "bam")
+              (not (s/includes? input "flo"))) "bamboo"
+             (s/includes? input "cha") "character"
+             (s/includes? input "dot") "dot"
+             (s/includes? input "dra") "dragon"
+             (s/includes? input "win") "wind"
+             (s/includes? input "sea") "season"
+             (s/includes? input "flo") "flower")
+       v (cond
+              (s/includes? input "1") 1
+              (s/includes? input "2") 2
+              (s/includes? input "3") 3
+              (s/includes? input "4") 4
+              (s/includes? input "5") 5
+              (s/includes? input "6") 6
+              (s/includes? input "7") 7
+              (s/includes? input "8") 8
+              (s/includes? input "9") 9
+              (s/includes? input "red") "red"
+              (s/includes? input "gre") "gre"
+              (s/includes? input "whi") "white"
+              (s/includes? input "eas") "east"
+              (s/includes? input "nor") "north"
+              (s/includes? input "sou") "south"
+              (s/includes? input "wes") "west"
+              (s/includes? input "spr") "spring"
+              (s/includes? input "sum") "summer"
+              (s/includes? input "aut") "autumn"
+              (s/includes? input "win") "winter"
+              (s/includes? input "plu") "plumb"
+              (s/includes? input "orc") "orchyd"
+              (s/includes? input "chr") "chrysantemum"
+              (and
+               (s/includes? input "ba")
+               (s/includes? input "fl")) "bamboo")]
+  (when (not (or (nil? n) (nil? v)))
+        (let [t {:name n :value v}
+              all-tiles (tiles n)]
+         (first (filter (partial = t) all-tiles))))))
 
 (defn char-is-number?
  "Returns true if the char is a number."
@@ -31,34 +61,7 @@
  (let [i (int c)]
   (and (>= i 48) (< i 58))))
 
-; make-get-suit-fn [tile-def-map] => Given a tile definition map, returns a fn that retrieves the suit of tile.
-(defn make-get-suit-fn
- "Given a tile definition map, returns a fn that retrieves the suit of tile."
- [tile-def-map]
- (fn get-suit [tile]
-  (:suit
-   (get tile-def-map
-    (keyword (str (first (name tile))))
-    nil))))
-
-; make-get-rank-fn [tile-def-map] => Given a tile definition map, returns a fn that retrieves the rank of tile.
-(defn make-get-rank-fn
- "Given a tile definition map, returns a fn that retrieves the rank of tile."
- [tile-def-map]
- (fn get-rank [tile]
-  (let [rank-coll (:ranks (get tile-def-map (keyword (str (first (name tile)))) nil))
-        c (second (name tile))
-        rank (if (char-is-number? c) (- (int c) 48) c)]
-   (get rank-coll rank nil))))
-
-; make-same-suit?-fn [tile-def-map] => Given a tile definition map, returns a fn that returns true if all the tiles supplied are of the same suit.
-(defn make-same-suit?-fn
- "Given a tile definition map, returns a fn that returns true if all the tiles supplied are of the same suit."
- [tile-def-map]
- (let [get-suit (make-get-suit-fn tile-def-map)]
-  (fn same-suit? [& tiles]
-   (let [[& suits] (map get-suit tiles)] ; Get suits for all the supplied tiles
-    (apply = suits)))))
+(def sort-tiles (partial sort-by (juxt :name :value)))
 
 ; make-interactions [inform-fn request-fn] => Returns a function that accepts a keyword. Abstraction of user interaction.
 (defn make-interactions [inform-fn request-fn]
@@ -74,16 +77,27 @@
 
 ; tiles [] => Returns a collection of all base-tiles of the mahjong game. A tile is represented by a 2-char keyword, the first char describes the suit (b for bamboo, w for wind f.e.) and the second char describes the rank (1 for one, n for north f.e.)
 (defn tiles
- "Returns a collection of all the base-tiles of the Mahjong game. Tiles are represented as keywords."
- []
- (reduce concat (map (fn compose-tile [s c] (map keyword (map str (repeat s) c)))
-                 ['b 'c 's 'd 'w]
-                 [(range 1 10) (range 1 10) (range 1 10) ['g 'r 'w] ['e 'n 's 'w]])))
+ "Returns a collection of tiles with name tile-name. (tile-name :bamboo) => list of all bamboos etc."
+ [tile-name]
+ (let [c (get {:dragon ["green" "red" "white"]
+               :wind ["east" "north" "south" "west"]
+               :season ["spring" "summer" "autumn" "fall"]
+               :flower ["bamboo" "chrysantemum" "orchid" "plumb"]}
+          (keyword tile-name)
+          (range 1 10))]
+  (map
+   (partial assoc {:name (name tile-name)} :value)
+   c)))
 
 ; new-wall [] => Returns a shuffled collection of tiles, representing a wall of Mahjong-tiles.
 (defn new-wall
- "Returns a shuffled collection of tiles, representing a wall of Mahjong-tiles."
- [] (shuffle (reduce concat (repeat 4 (tiles)))))
+ "Returns a shuffled collection of tiles, representing a wall of Mahjong tiles."
+ []
+ (let [all-single-tiles (reduce concat (map tiles [:bamboo :character :dot :dragon :wind]))
+       bonus-tiles (reduce concat (map tiles [:season :flower]))]
+  (shuffle (concat
+            (reduce concat (repeat 4 all-single-tiles)) ; Repeat suited tiles 4 times
+            bonus-tiles)))) ; Bonus tiles not repeated
 
 ; make-players [] => Returns a collection of maps representing players. It contains a name, a hand and revealed (sets).
 (defn new-players
@@ -101,7 +115,7 @@
        player (first (:players g))
        hand (concat (:hand player) drawn-tiles)
        ; Let the new player be the old player with the drawn-tiles concatenated to its hand
-       new-player (assoc player :hand (sort hand))]
+       new-player (assoc player :hand (sort-tiles hand))]
   (assoc g
    :players (conj (rest (:players g)) new-player) ; Replace the first player
    :wall c)))
@@ -133,18 +147,34 @@
 
 ; new-game [] => Returns a map with a closure around it that refs to the newly created game.
 (defn new-game
- "Returns a fn with a closure around a map representing a game. The returned function accepts one arg (k) which should be a keyword. It returns the function matching with the keyword."
- []
- (let [g (atom {:wall (new-wall) :players (new-players) :discard nil})]
-  (fn new-game [k]
-   "Things you can do"
-   (k {; GETTERS
-       :players (fn game-get-players [] (:players @g)),
-       :hand (fn game-get-hand [] (:hand (first (:players @g)))),
-       :wall (fn game-get-wall [] (:wall @g)),
-       :discard-pile (fn game-get-discard-pile [] (sort (frequencies (:discard @g)))), ; sort the discard-pile for better view
+           "Returns a fn with a closure around a map representing a game. The returned function accepts one arg (k) which should be a keyword. It returns the function matching with the keyword."
+           []
+           (let [g (atom {:wall (new-wall) :players (new-players) :discard nil})]
+            (fn new-game [k]
+             "Things you can do"
+             (k {; GETTERS
+                 :players (fn game-get-players [] (:players @g)),
+                 :hand (fn game-get-hand [] (:hand (first (:players @g)))),
+                 :wall (fn game-get-wall [] (:wall @g)),
+                 :discard-pile (fn game-get-discard-pile [] (sort (frequencies (:discard @g)))), ; sort the discard-pile for better view
        ; SETTERS
-       :draw-tile (fn game-draw-tile [] (swap! g (partial draw-tiles 1))),
-       :draw-tiles (fn game-draw-tiles [n] (swap! g (partial draw-tiles n))),
-       :discard-tile (fn game-discard-tile [t] (swap! g (partial discard-tile t)))
-       :rotate-players (fn game-rotate-players [] (swap! g (partial rotate-players)))}))))
+                 :draw-tile (fn game-draw-tile [] (swap! g (partial draw-tiles 1))),
+                 :draw-tiles (fn game-draw-tiles [n] (swap! g (partial draw-tiles n))),
+                 :discard-tile (fn game-discard-tile [t] (swap! g (partial discard-tile t))),
+                 :rotate-players (fn game-rotate-players [] (swap! g (partial rotate-players)))}))))
+
+(defn draw-hands
+ [game]
+ (loop [g game]
+  (if (not (empty? ((g :hand))))
+   g
+   (recur (dosync
+           ((g :draw-tiles) 13)
+           ((g :rotate-players))
+           g)))))
+
+(defn main
+ [game]
+ (dosync
+  (draw-hands game)
+  game))
